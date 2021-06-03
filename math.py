25e57ea58726bb711a8b96c3b103819042122cd7
NehaTuniya <nehapt17.extc@coep.ac.in>
2021-06-03 15:08:46 +0530

    added sub implementation on main branch with error code

--- math.py
+++ math.py
@@ -3,8 +3,11 @@ def add(x,y):
 	return x+y
 
 #sub implementation
-def subtract(x,y):
-	pass
+def subtract(x,y):	#main branch
+	if y>x:
+		return error
+	else:
+		return x-y
 
 #multiply implementation
 def multiply(x,y):
